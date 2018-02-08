75546ffecec276664dbfdc3892deb96cde4ad9c3
unknown <amanda.wisniowski@gmail.com>
2018-02-08 14:25:13 -0600

    fixed portfolio, needs logo

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+  
+
+    $('.parallax').parallax();
+      
+      $('.carousel').carousel();
+      
+      $('.modal').modal();
+      // DROPDOWNS
+      $(".dropdown-button").dropdown(
+        {
+          belowOrigin: true
+        }
+      );
+      // TABS
+      $('ul.tabs').tabs();
+      // SCROLLSPY
+      $('.scrollspy').scrollSpy();
+      //SIDENAV
+      $(".button-collapse").sideNav();
+  
+    });
+      
