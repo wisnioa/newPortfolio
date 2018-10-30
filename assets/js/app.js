7234bbb1925aaa097586dcc2b9fc8ed80e01e606
unknown <amanda.wisniowski@gmail.com>
2018-10-30 13:40:03 -0500

    overhaul of portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,97 @@
 
-$(document).ready(function(){
-  
-    $('.parallax').parallax();
-    
-      $('.carousel').carousel();
-      
-      $('.modal').modal();
-      // DROPDOWNS
-      $(".dropdown-button").dropdown(
-        {
-          belowOrigin: true,
-          hover: false
-        }
-      );
 
-      // var options = [
-   
-      //   {selector: '#card1', offset: 600, callback: function(el) {
-      //     Materialize.fadeInImage($(el));
-      //   },
-      // // ];
-      // Materialize.scrollFire(options);
-      // TABS
-      $('ul.tabs').tabs();
-      // SCROLLSPY
-      $('.scrollspy').scrollSpy();
-      //SIDENAV
+
+$(document).ready(function () {
+
+
+  // $('.sidenav').sidenav();
+  $('.carousel').carousel();
+  
+  
+});
+
+(function() {
 
   
-  // Show sideNav
-  $('.button-collapse').sideNav();
- 
- 
-	
 
-    });
-      
+
+  var sr = ScrollReveal({ reset: true, duration: 5100 });
+
+  // Custom Settings
+  sr.reveal('.skill-1');
+  sr.reveal('.skill-2');
+  sr.reveal('.skill-3');
+  sr.reveal('.skill-4');
+  sr.reveal('.skill-5');
+  sr.reveal('.skill-6');
+  sr.reveal('.skill-7');
+  sr.reveal('.skill-8');
+  sr.reveal('.skill-9');
+  sr.reveal('.skill-10');
+  sr.reveal('.skill-11');
+  sr.reveal('.skill-12');
+  sr.reveal('.skill-13');
+  sr.reveal('.skill-14');
+  sr.reveal('.skill-15');
+  sr.reveal('.skill-16');
+
+  
+  
+})();
+
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Hide navbar when modals trigger
+  $('.portfolio-modal').on('show.bs.modal', function(e) {
+    $('.navbar').addClass('d-none');
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $('.navbar').removeClass('d-none');
+  })
+
+})(jQuery); 
+
+
+
+
+
